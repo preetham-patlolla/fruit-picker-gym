81a1d139074ab99310ef65cf38da4538495cc0cb
Preetham Patlolla <patlolla.preetham1410@gmail.com>
2025-02-03 20:08:16 -0500

    Adjusted the render methods' camera view to focus on the source bin alone

--- fruit_picker_gym/envs/custom_fruit_picker_gym_env.py
+++ fruit_picker_gym/envs/custom_fruit_picker_gym_env.py
@@ -274,10 +274,10 @@ class FruitPickerEnv(gym.Env):
 
         logger.debug(f"Started rendering the image")
         # Place the camera ta a desired position and orientation in the environment
-        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0,-0.4,0.2],
-                                                          distance=2,
-                                                          yaw=-60,
-                                                          pitch=-70,
+        view_matrix = p.computeViewMatrixFromYawPitchRoll(cameraTargetPosition=[0.1,-0.6,0.2],
+                                                          distance=0.7,
+                                                          yaw=0,
+                                                          pitch=-90,
                                                           roll=0,
                                                           upAxisIndex=2)
 
