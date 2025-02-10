0a4f5960e3d52f19bd7ac638a53315768a17f15b
Preetham Patlolla <patlolla.preetham1410@gmail.com>
2025-02-09 21:11:52 -0500

    Removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import gymnasium
-import fruit_picker_gym
-env = gymnasium.make('fruitpicker-env-01')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
